9645aab0ef0e90d4a3157ef4bf8be96e95860c59
Mrgoku348 <carlos.merma@utec.edu.pe>
2019-11-26 09:59:57 -0500

    m

--- app/src/main/java/cs2901/utec/chat_mobile/LoginActivity.java
+++ app/src/main/java/cs2901/utec/chat_mobile/LoginActivity.java
@@ -1,25 +1,23 @@
 package cs2901.utec.chat_mobile;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
-import java.util.Map;
-import java.util.HashMap;
-import org.json.JSONObject;
 
-import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
-import android.content.Intent;
+
 import org.json.JSONException;
-import android.view.View;
-
-
+import org.json.JSONObject;
+import java.util.HashMap;
+import java.util.Map;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -33,61 +31,64 @@ public class LoginActivity extends AppCompatActivity {
         Toast.makeText(this, message, Toast.LENGTH_LONG).show();
     }
 
-    public void onBtnLoginClicked(View view) {
-        // 1. Getting username and password inputs from view
-        EditText txtUsername = (EditText) findViewById(R.id.txtUsername);
-        EditText txtPassword = (EditText) findViewById(R.id.txtPassword);
+    public void onBtnLoginClicked(View view){
+        //1.  Getting username and password (from the view)
+        EditText txtUsername = (EditText)findViewById(R.id.txtUsername);
+        EditText txtPassword = (EditText)findViewById(R.id.txtPassword);
         String username = txtUsername.getText().toString();
         String password = txtPassword.getText().toString();
 
-        // 2. Creating a message from user input data
+        //2.  Creating a message using user input
         Map<String, String> message = new HashMap<>();
         message.put("username", username);
         message.put("password", password);
 
-        // 3. Converting the message object to JSON string (jsonify)
+        //3.  Converting the message object to JSON string (jsonify)
         JSONObject jsonMessage = new JSONObject(message);
 
-        // 4. Sending json message to Server
+
+        //4.  Sending json message to the server
+        //4.1. Install volley
+        //4.2. Create request object
         JsonObjectRequest request = new JsonObjectRequest(
-            Request.Method.POST,
-            "http://10.0.2.2:8080/authenticate",
-            jsonMessage,
-            new Response.Listener<JSONObject>() {
-                @Override
-                public void onResponse(JSONObject response) {
-                    //TODO
-                    try {
-                        String message = response.getString("message");
-                        if(message.equals("Authorized")) {
-                            showMessage("Authenticated");
+                Request.Method.POST,
+                "http://10.0.2.2:8000/authenticate",
+                jsonMessage,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        //TODO Qué hacer cuando el server responda
+                        showMessage("Authorized!");
+                        try {
+                            String username = response.getString("username");
+                            int user_id = response.getInt("user_id");
+                            goToContactsActivity(user_id, username);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
                         }
-                        else {
-                            showMessage("Wrong username or password");
-                        }
-                        showMessage(response.toString());
-                    }catch (Exception e) {
-                        e.printStackTrace();
-                        showMessage(e.getMessage());
+
+
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        //TODO Qué hacer cuando ocurra un error
+                        showMessage("Unauthorized!!!");
                     }
                 }
-            },
-            new Response.ErrorListener() {
-                @Override
-                public void onErrorResponse(VolleyError error) {
-                    error.printStackTrace();
-                    if( error instanceof  AuthFailureError ){
-                        showMessage("Unauthorized");
-                    }
-                    else {
-                        showMessage(error.getMessage());
-                    }
-                }
-            }
         );
 
+        //5. Send Request to the Server
         RequestQueue queue = Volley.newRequestQueue(this);
         queue.add(request);
+
     }
 
+    private void goToContactsActivity(int user_id, String username) {
+        Intent intent = new Intent(this, ContactsActivity.class);
+        intent.putExtra("user_id", user_id);
+        intent.putExtra("username", username);
+        startActivity(intent);
+    }
 }
